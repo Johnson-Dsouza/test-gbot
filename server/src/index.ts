4fa6d5c7cc87141c9c731954961a5193380d3daf
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-09-06 15:46:20 +0530

    introducing express.js in the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,22 @@ require("dotenv").config();
 const { handleListeners } = require("./listeners");
 
 const { App } = require("@slack/bolt");
+const express = require("express");
+
+const app = express();
 
 //initializing the app
-const app = new App({
+const boltApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGINING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
-handleListeners(app);
+handleListeners(boltApp);
 
-app.start(3000);
+boltApp.start(8080);
 
-
-
+app.listen(8000, () => {
+  console.log("Server is running");
+});
