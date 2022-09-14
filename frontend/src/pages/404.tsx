56f65f4bcfbdd6a17a0ffba77a91fdd624af7f7d
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-09-14 10:21:29 +0530

    initial setup

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <main>
+      <h3>Sorry, an error occured</h3>
+    </main>
+  )
+}
+
+export default NotFoundPage
+
+
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <main>
+     <h1>G-BOT</h1>
+    </main>
+  )
+}
+
+export default IndexPage
+
+
