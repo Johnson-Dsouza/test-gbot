8a357e7736b738f8c562eafa6fc307114353b0d8
Ayush Srivastava <apple@apples-MacBook-Air.local>
2022-09-15 10:12:21 +0530

    removed empty lines

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 const NotFoundPage = () => {
   return (
     <main>
@@ -9,5 +8,3 @@ const NotFoundPage = () => {
 }
 
 export default NotFoundPage
-
-
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-
-
 const IndexPage = () => {
   return (
     <main>
@@ -10,5 +8,3 @@ const IndexPage = () => {
 }
 
 export default IndexPage
-
-
