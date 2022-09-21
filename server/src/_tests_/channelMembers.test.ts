47c91a34acfcf2772a670d24c98a3be3f0ec0ec6
ayushdevelop <see.ayush.here.2@gmail.com>
2022-09-21 22:48:26 +0530

    removed unwanted imports

--- server/src/_tests_/channelMembers.test.ts
+++ server/src/_tests_/channelMembers.test.ts
@@ -1,11 +1,8 @@
 export {};
 
 const request = require("supertest");
-const express = require("express");
 
 const app = require("../server");
-
-app.use(express.json());
 
 const channelMembersRouter = require("../routes/api/channelMembers");
 
